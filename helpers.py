f53b3cbcc23bbe3a9231d78ae0f001634ab58371
Jacob Grisham <jacob.d.grisham@gmail.com>
2021-06-13 16:26:33 -0700

    update API call in lookup() function in helpers.py. iex cloud changed the base url for the API.

--- helpers.py
+++ helpers.py
@@ -41,7 +41,7 @@ def lookup(symbol):
     # Contact API
     try:
         api_key = os.environ.get("API_KEY")
-        response = requests.get(f"https://cloud-sse.iexapis.com/stable/stock/{urllib.parse.quote_plus(symbol)}/quote?token={api_key}")
+        response = requests.get(f"https://cloud.iexapis.com/stable/stock/{urllib.parse.quote_plus(symbol)}/quote?token={api_key}")
         response.raise_for_status()
     except requests.RequestException:
         return None
