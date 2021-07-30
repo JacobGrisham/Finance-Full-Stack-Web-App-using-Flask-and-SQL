2cf863a82d09826cac8d1e01bee3c673a35ae111
Jacob Grisham <jacob.d.grisham@gmail.com>
2021-07-29 17:51:07 -0700

    update login & register html, styles.css, index.js
    add Bootstrap custom client-side validation for the login and register forms

--- static/index.js
+++ static/index.js
@@ -4,3 +4,47 @@ function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 
 gtag('config', 'G-36KQSD0BWH');
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+  'use strict'
+
+  window.addEventListener('load', function() {
+    // Fetch all the forms we want to apply custom validation styles
+    var inputs = document.getElementsByClassName('form-control')
+
+    // Loop over each input and watch blue event
+    var validation = Array.prototype.filter.call(inputs, function(input) {
+
+      input.addEventListener('blur', function(event) {
+        // reset
+        input.classList.remove('is-invalid')
+        input.classList.remove('is-valid')
+
+        if (input.checkValidity() === false) {
+            input.classList.add('is-invalid')
+        }
+        else {
+            input.classList.add('is-valid')
+        }
+      }, false);
+    });
+  }, false);
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll('.needs-validation');
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+
+        form.classList.add('was-validated')
+      }, false)
+    })
+
+})()
